8acea4ac9ab1a017f93ac2ce3fb74fa41cf04a2a
chandank <chandank@meditab.com>
2018-05-30 15:45:22 +0530

    Recipes setup with looping and images

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Chicken Tikka', 'This is the recipe for Chicken tikka', 'http://www.lakesideindianrestaurant.com.au/wp-content/uploads/2017/02/chikantikka.jpg'),
+    new Recipe('Paneer Tikka','This is the recipe for Paneer tikka','http://www.archanaskitchen.com//images/archanaskitchen/1-Author/Neha_Mathur/Achari_Paneer_Tikka_Recipe_Party_Food.JPG')
+  ];
   constructor() { }
 
   ngOnInit() {
